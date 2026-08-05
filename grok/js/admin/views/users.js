ca6377be8c9447862302085a90c47ce4fcafe1b4
bofutz <hahagw2016+github@gmail.com>
2026-08-05 11:21:50 +0800

    Create users.js

---
+++ grok/js/admin/views/users.js
@@ -0,0 +1,13 @@
+export const UsersView = {
+  name: "AdminUsers",
+  props: {
+    fetchAdmin: { type: Function, required: true },
+    showToast: { type: Function, required: true },
+  },
+  template: `
+    <div class="bg-white rounded-xl border p-8 text-center text-slate-400">
+      <i class="fa-solid fa-users text-3xl mb-3 theme-text"></i>
+      <p class="text-sm">用户管理模块（下一步填充）</p>
+    </div>
+  `,
+};
